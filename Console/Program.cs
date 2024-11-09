e0161dc17f3dcc5af699fdfe3183ddda134a46e3
Jose Chacon <jcurias123@gmail.com>
2024-11-08 23:10:24 -0600

    Implement power method

--- Console/Program.cs
+++ Console/Program.cs
@@ -81,7 +81,7 @@ namespace GithubActionsLab
             return double.Parse(x) / double.Parse(y);
         }
 
-        // Implement this method following a similar pattern as above
+        // Implement this method following a similar pattern as above. ok
         public static double Power(string x, string y)
         {
             return Math.Pow(double.Parse(x), double.Parse(y));
